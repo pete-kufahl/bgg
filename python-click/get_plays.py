a4f4b769f166f19a65b70a9f2204b4d38cd05fa8
Peter Kufahl <pkufahl@gmail.com>
2023-07-04 21:02:50 -0700

    error message - see if this happens for multiple game IDs

--- python-click/get_plays.py
+++ python-click/get_plays.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import time
 import xml.etree.ElementTree as ET
+from collections import defaultdict
 
 debug = False
 print_rank = True
@@ -19,6 +20,9 @@ def extract_game_data(xml_str, debug=False):
         if debug:
             i += 1
             print(f'game {i}: {game_id} ...')
+        if not game.find('name'):
+            print(f'no name attribute found for game-id {game_id}, despite the API request\'s 200 return! Try running again?')
+            continue
         name = game.find('name').text
         rank = game.find('.//rank[@type="subtype"]').attrib['value']
         numplays = game.find('numplays').text
@@ -55,97 +59,96 @@ def map_rating(rating, spaces=1):
         1: '[BGCOLOR=#FF0000] [b]' + str(rating) + '[/b] [/BGCOLOR]'
     }.get(k, '[BGCOLOR=#A3A3A3] [b]--[/b] [/BGCOLOR]') + spacer
 
-# Retrieve command line arguments
-if len(sys.argv) < 3:
-    print("Please provide mindate (yyyy-mm-dd) and maxdate (yyyy-mm-dd) parameters, and (optionally) your BGG username as the third parameter.")
-    sys.exit(1)
+if __name__ == "__main__":
+    # Retrieve command line arguments
+    if len(sys.argv) < 3:
+        print("Please provide mindate (yyyy-mm-dd) and maxdate (yyyy-mm-dd) parameters, and (optionally) your BGG username as the third parameter.")
+        sys.exit(1)
 
-mindate = sys.argv[1]
-maxdate = sys.argv[2]
+    mindate = sys.argv[1]
+    maxdate = sys.argv[2]
 
-if len(sys.argv) == 4:
-    YOUR_BGG_NAME = sys.argv[3]
+    if len(sys.argv) == 4:
+        YOUR_BGG_NAME = sys.argv[3]
 
-# API request URL
-url = f'https://boardgamegeek.com/xmlapi2/plays?username={YOUR_BGG_NAME}&mindate={mindate}&maxdate={maxdate}&type=thing&subtype=boardgame&brief=1'
+    # API request URL
+    url = f'https://boardgamegeek.com/xmlapi2/plays?username={YOUR_BGG_NAME}&mindate={mindate}&maxdate={maxdate}&type=thing&subtype=boardgame&brief=1'
 
-# Make the API request
-response = requests.get(url)
-
-while response.status_code == 202:
-    print("Waiting for response...")
-    time.sleep(0.33)
+    # Make the API request
     response = requests.get(url)
 
-if response.status_code != 200:
-    print(f'url request failed with code {response.status_code}')
-    exit(1)
+    while response.status_code == 202:
+        print("Waiting for response...")
+        time.sleep(0.33)
+        response = requests.get(url)
 
-xml_str = response.text
+    if response.status_code != 200:
+        print(f'url request failed with code {response.status_code}')
+        exit(1)
 
-# Parse the XML string
-root = ET.fromstring(xml_str)
+    xml_str = response.text
 
-# Extract game IDs and number of plays
-games = root.findall('.//play/item')
-plays_per_game = {}
+    # Parse the XML string
+    root = ET.fromstring(xml_str)
 
-for game in games:
-    game_id = game.attrib['objectid']
-    play_count = game.attrib.get('quantity', 1)
-    play_count = int(play_count)
-    if game_id in plays_per_game:
-        plays_per_game[game_id] += play_count
-    else:
-        plays_per_game[game_id] = play_count
+    # Extract game IDs and number of plays
+    gameplays = root.findall('.//play')
+    plays_per_game = defaultdict(int)
 
-# look up the game rank and name for each id
-game_ids = [int(k) for k in plays_per_game.keys()]
-game_ids_str = ",".join(map(str, game_ids))
+    for gameplay in gameplays:
+        game = gameplay.find('item')
+        assert game
+        game_id = game.attrib['objectid']
+        play_count = gameplay.attrib.get('quantity', 1)
+        plays_per_game[game_id] += int(play_count)
 
-# Print the number of plays per game
-if debug:
-    for game_id, play_count in plays_per_game.items():
-        print(f"Game ID: {game_id}, Plays: {play_count}")
+    # look up the game rank and name for each id
+    game_ids = [int(k) for k in plays_per_game.keys()]
+    game_ids_str = ",".join(map(str, game_ids))
 
-url2 = f'https://boardgamegeek.com/xmlapi2/collection?username={YOUR_BGG_NAME}&id=' + game_ids_str + '&stats=1'
-# Make the API request
-if debug:
-    print(url2 + ' ...') 
-response = requests.get(url2)
+    # Print the number of plays per game
+    if debug:
+        for game_id, play_count in plays_per_game.items():
+            print(f"Game ID: {game_id}, Plays: {play_count}")
 
-while response.status_code == 202:
-    print("Waiting for response...")
-    time.sleep(0.33)
-    response = requests.get(url)
+    url2 = f'https://boardgamegeek.com/xmlapi2/collection?username={YOUR_BGG_NAME}&id=' + game_ids_str + '&stats=1'
+    # Make the API request
+    if debug:
+        print(url2 + ' ...') 
+    response = requests.get(url2)
 
-if response.status_code != 200:
-    print(f'url request failed with code {response.status_code}')
-    exit(1)
+    while response.status_code == 202:
+        print("Waiting for response...")
+        time.sleep(0.33)
+        response = requests.get(url)
 
-game_bgg_data = extract_game_data(response.text, debug=False)
-for game in game_bgg_data:
-    game['play_count'] = plays_per_game[game['game_id']]
-game_bgg_data.sort(key=lambda x: x['play_count'], reverse=True)
+    if response.status_code != 200:
+        print(f'url request failed with code {response.status_code}')
+        exit(1)
 
-for game in game_bgg_data:
-    rating = game['rating_value']
-    rating_str = map_rating(rating, 1)
-    name = game['name']
-    rank = game['rank']
-    plays = game['play_count']
-    plays_str = f' x{plays}' if int(plays) > 1 else ''
-    total = game['numplays']
-    if int(plays) == int(total):
-        total_str = '[b][COLOR=#FF0000][size=7]NEW![/size][/COLOR][/b]'
-    else:
-        total_str = f'[size=7]({total} so far)[/size]'
-    if print_rank:
-        if 'Not' in str(rank):
-            rank_str = '[size=8]' + 'unranked' + ' [/size]'
+    game_bgg_data = extract_game_data(response.text, debug=False)
+    for game in game_bgg_data:
+        game['play_count'] = plays_per_game[game['game_id']]
+    game_bgg_data.sort(key=lambda x: x['play_count'], reverse=True)
+
+    for game in game_bgg_data:
+        rating = game['rating_value']
+        rating_str = map_rating(rating, 1)
+        name = game['name']
+        rank = game['rank']
+        plays = game['play_count']
+        plays_str = f' x{plays}' if int(plays) > 1 else ''
+        total = game['numplays']
+        if int(plays) == int(total):
+            total_str = '[b][COLOR=#FF0000][size=7]NEW![/size][/COLOR][/b]'
         else:
-            rank_str = '[size=8]' + str(rank).rjust(8,' ') + ' [/size]'
-        print(f'[c]{rank_str} [/c]{rating_str} {name}{plays_str} {total_str}')
-    else:
-        print(f'{rating_str} {name}{plays_str} {total_str}')
+            total_str = f'[size=7]({total} so far)[/size]'
+        if print_rank:
+            if 'Not' in str(rank):
+                rank_str = '[size=8]' + 'unranked' + ' [/size]'
+            else:
+                rank_str = '[size=8]' + str(rank).rjust(8,' ') + ' [/size]'
+            print(f'[c]{rank_str} [/c]{rating_str} {name}{plays_str} {total_str}')
+        else:
+            print(f'{rating_str} {name}{plays_str} {total_str}')
 
