85127a4dbc3e459f314418040edfe347a92729e1
Peter Kufahl <pkufahl@gmail.com>
2023-12-17 00:21:26 -0700

    new-to-me command

--- node-cli/get_plays.js
+++ node-cli/get_plays.js
@@ -30,6 +30,41 @@ program
             process.exit(1);
         }
     })
-    .exitOverride();
+    
+program
+    .command('new')
+    .description('Reports the new-to-you games of the time period')
+    .requiredOption('-u, --username <username>', 'The username to include in the URL')
+    .option('-s, --startdate <date>', 'Start date (yyyy-MM-dd) for data fetching. Default: Seven days ago.')
+    .option('-e, --enddate <date>', 'End date (yyyy-MM-dd) for data fetching. Default: Now.')
+    .option('-l, --links', 'output game names as BGG links')
+    .option('-r, --ranks', 'include BGG ranks in output')
+    .option('-c, --comments', 'include comments from the user collection data')
+    .option('-i, --image_placeholders', 'include placeholder lines for BGG images')
+    .option('-d, --debug', 'output extra debugging')
+    .action(async (options) => {
+        try {
+            const { username, startdate, enddate, links, ranks, comments, image_placeholders, debug } = options;
+            const plays_per_game = await fetchGamesPlayed(username, startdate, enddate);
+            const details = await fetchGameDetails(username, plays_per_game);
+            const new_to_me = details.filter(obj => obj.play_count === parseInt(obj.numplays, 10));
+
+            for (const game_info of new_to_me) {
+                formatted = formatGameInfo(game_info, links, ranks)
+                console.log(formatted)
+
+                if (image_placeholders) {
+                    console.log(`\n[imageID=PLACEHOLDER medium][size=7]image by CREDIT[/size]\n`);
+                }   
+                if (comments) {
+                    console.log(game_info.comment);
+                    console.log();
+                }
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+            process.exit(1);
+        }
+    })
 
 program.parse(process.argv);
